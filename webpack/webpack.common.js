1f57912a4c8e10f12a460c3b17d88b83d07046eb
Jose Luis Sandoval <jl_sandoval_salas@hotmail.com>
2018-06-14 09:16:38 -0500

    add mixins head

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const htmlWebpack = new HtmlWebpackPlugin({
-    template: './app/index.pug',
+    template: './app/views/index.pug',
     filename: 'index.html'
 });
 
